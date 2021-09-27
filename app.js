138c3fe76ed2dad74baa6438e55db0c446f18477
Hakoeda <kurikithune@gmail.com>
2021-09-27 09:56:00 +0900

    提案：年代取得関数の修正提案とコードフォーマット

--- app.js
+++ app.js
@@ -3,47 +3,47 @@
 const generations = [
     {
         name: "10歳未満",
-        age_id_range: { from: 1001, to: 1010}
+        age_id_range: { from: 1001, to: 1010 }
     },
     {
-        name:"10代",
-        age_id_range: { from: 1011, to: 1020}
+        name: "10代",
+        age_id_range: { from: 1011, to: 1020 }
     },
     {
-        name:"20代",
-        age_id_range: { from: 1021, to: 1030}
+        name: "20代",
+        age_id_range: { from: 1021, to: 1030 }
     },
     {
-        name:"30代",
-        age_id_range: { from: 1031, to: 1040}
+        name: "30代",
+        age_id_range: { from: 1031, to: 1040 }
     },
     {
-        name:"40代",
-        age_id_range: { from: 1041, to: 1050}
+        name: "40代",
+        age_id_range: { from: 1041, to: 1050 }
     },
     {
-        name:"50代",
-        age_id_range: { from: 1051, to: 1060}
+        name: "50代",
+        age_id_range: { from: 1051, to: 1060 }
     },
     {
-        name:"60代",
-        age_id_range: { from: 1061, to: 1070}
+        name: "60代",
+        age_id_range: { from: 1061, to: 1070 }
     },
     {
-        name:"70代",
-        age_id_range: { from: 1071, to: 1080}
+        name: "70代",
+        age_id_range: { from: 1071, to: 1080 }
     },
     {
-        name:"80代",
-        age_id_range: { from: 1081, to: 1090}
+        name: "80代",
+        age_id_range: { from: 1081, to: 1090 }
     },
     {
-        name:"90代",
-        age_id_range: { from: 1091, to: 1100}
+        name: "90代",
+        age_id_range: { from: 1091, to: 1100 }
     },
     {
-        name:"100歳以上",
-        age_id_range: { from: 1101, to: 1101}
+        name: "100歳以上",
+        age_id_range: { from: 1101, to: 1101 }
     },
 ];
 
@@ -52,12 +52,11 @@ const fs = require('fs');
 const readline = require('readline');
 const rs = fs.createReadStream('./popu_source.csv');
 const dest = fs.createWriteStream('popu_ranking.txt', 'utf8'); // 出力先
-const rl = readline.createInterface({input:rs,output:{} });
+const rl = readline.createInterface({input: rs, output: {} });
 const generationPopulationMap = new Map(); // key:世代　value:世代合計人口
 
-//データ取り込みはダブルコーテーションをreplaceして実行
-rl.on('line',lineString =>{
-    lineString = lineString.replace(/\"/g,"");
+rl.on('line', lineString => {
+    lineString = lineString.replace(/\"/g,""); //ダブルコーテーションを除去
     const columns = lineString.split(",");
 
     const gender = columns[1]; //男女別
@@ -67,29 +66,32 @@ rl.on('line',lineString =>{
     const population = parseInt(columns[11]);　//人口数
 
     //男女計と総人口に絞る
-    if(gender !== '男女計'　|| popuAggregate !== '総人口'){
+    if (gender !== '男女計' || popuAggregate !== '総人口') {
         return;
     }
     // 総数を弾く
     if (age === '総数') {
-        return
+        return;
     }
+    
     // 年代を取得
-    const generationName = getGenerationName(ageId);
+    const generationName = getGeneration(ageId).name;
     // 現時点での年代の人口を取得
     const generationPopulation = generationPopulationMap.get(generationName) || 0;
     // 年代に加算する
     generationPopulationMap.set(generationName, generationPopulation + population);
 });
 
-rl.on('close',() => {
+rl.on('close', () => {
     // 年代が多い順にソートする
-    const sortedGenerationPopulationMap = Array.from(generationPopulationMap).sort((pair1, pair2) => {
+    const sortedGenerationPopulationMap = Array.from(generationPopulationMap)
+    .sort((pair1, pair2) => {
         return pair2[1] - pair1[1];
     });
+    
     // 結果を出力
-    let rank = 1;
     dest.write("2019年10月1日現在の年代別総人口ランキング [千人]\n");
+    let rank = 1;
     for (let generationArray of sortedGenerationPopulationMap) {
         const generation = generationArray[0];
         const population = generationArray[1];
@@ -98,11 +100,17 @@ rl.on('close',() => {
     }
 });
 
-function getGenerationName(ageId) {
-    for(const generation of generations){
-        if(generation.age_id_range.from <= ageId && ageId <= generation.age_id_range.to){
-            return generation.name;
+/**
+ * 年代の取得
+ * @param {Number} ageId 
+ * @returns {Object} 年代オブジェクト
+ * @throws {Error} 範囲外のageIdエラー
+ */
+function getGeneration(ageId) {
+    for (const generation of generations) {
+        if (generation.age_id_range.from <= ageId && ageId <= generation.age_id_range.to) {
+            return generation;
         }
     }
-    return null;
+    throw new Error("範囲外の年齢IDです");
 }
